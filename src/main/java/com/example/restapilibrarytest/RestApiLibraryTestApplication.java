6e59224c4aad48b00cf4def357e096e49e011ba0
Alvin <vktmzkf1987@naver.com>
2021-09-21 09:57:40 +0900

    Initial commit

---
+++ src/main/java/com/example/restapilibrarytest/RestApiLibraryTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapilibrarytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiLibraryTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiLibraryTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restapilibrarytest/RestApiLibraryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapilibrarytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiLibraryTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
